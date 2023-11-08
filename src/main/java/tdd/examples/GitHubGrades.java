ba778d390aed4c50d5d379f209e245cba3f714c0
Logan Breaux <113488498+swisscheesey@users.noreply.github.com>
2023-11-08 17:46:16 -0600

    Update GitHubGrades.java

--- src/main/java/tdd/examples/GitHubGrades.java
+++ src/main/java/tdd/examples/GitHubGrades.java
@@ -1,13 +1,18 @@
 package tdd.examples;
 
 import java.util.Collection;
+import java.util.*;
 
 public class GitHubGrades {
+    private Map<String, Collection<String>> gradeMap = new HashMap<>();
 
     public void addGrade(String fred, String number) {
+        if(!gradeMap.containsKey(fred)){
+            gradeMap.put(studentName, new ArrayList<>());
     }
-
+        gradeMap.get(studentName).add(grade);
+    }
     public Collection<String> getGrades(String fred) {
-        return null;
+        return gradeMap.getOrDefault(studentName, new ArrayList<>());
     }
 }
