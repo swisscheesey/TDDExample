ca9bd282ebf61529ac08fce82c253e228beb64d8
Logan Breaux <113488498+swisscheesey@users.noreply.github.com>
2023-11-08 17:38:54 -0600

    Update GithubToEmailTracker.java

--- src/main/java/tdd/examples/GithubToEmailTracker.java
+++ src/main/java/tdd/examples/GithubToEmailTracker.java
@@ -2,10 +2,11 @@ package tdd.examples;
 
 public class GithubToEmailTracker {
     public GithubToEmailTracker(){
-
+        private Map<String, String> emailToGithubMap = new HashMap<>();
     }
 
     public String getEmail(String gitHubId){
+        private
         return null;
     }
 
